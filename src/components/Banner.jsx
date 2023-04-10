2837dbcb4bf79c775f4b2974847ed59e5d00bb00
Hasib24 <hasibislam1488@gmail.com>
2023-04-10 10:09:11 +0600

    add bannaer style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import BannerPic from '../assets/img/bannerPic.png'
 
 const Banner = () => {
+
+    
+    const siteLogoStyle = {
+        backgroundImage: 'linear-gradient(90deg, #7E90FE 0%, #9873FF 100%)',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor:'transparent'
+
+    }
+
     return (
-        <div className='container mx-auto'>
-            the banner page!
+        <div className='container mx-auto px-4 md:flex md:flex-row-reverse justify-between'>
+            <div>
+                <img src={BannerPic} alt="Banner photo" />
+            </div>
+            <div className='border md:w-1/2 flex flex-col justify-center'>
+                <h1 className='text-5xl font-bold tracking-wider'>Every small step <br /> count toward your <br /> <span style={siteLogoStyle}>Deram Job!</span></h1>
+                <p className='text-slate-600 my-5'>
+                    Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
+                </p>
+            </div>
         </div>
     );
 };
