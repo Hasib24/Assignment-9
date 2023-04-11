b9aa95c7e2097b3823273f467865c62777b26ce3
Hasib24 <hasibislam1488@gmail.com>
2023-04-11 22:55:48 +0600

    update error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Error = () => {
     return (
-        <div>
-            404 page not found. this is error page!
+        <div className='flex flex-col container mx-auto text-center items-center'>
+            <img className='h-[75vh] w-[75vw] object-cover object-center' src="https://img.freepik.com/free-vector/internet-network-warning-404-error-page-file-found-web-page_1150-48326.jpg?w=740&t=st=1681209547~exp=1681210147~hmac=00cf9bba3a99876db5eea28e0748f49b8e1039ee59d8533c4f2f1a1563d8c69c" alt="" />
+        
         </div>
     );
 };
