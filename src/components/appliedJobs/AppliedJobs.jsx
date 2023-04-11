3bd4d27fff042f46b91f45074d2809a53d342a7f
Hasib24 <hasibislam1488@gmail.com>
2023-04-12 02:18:57 +0600

    filtered jobs successfuly

--- src/components/appliedJobs/AppliedJobs.jsx
+++ src/components/appliedJobs/AppliedJobs.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import AppliedJobCard from './AppliedJobCard';
 
 const AppliedJobs = () => {
+
+    let [filterJobBy, setFilterJobBy ] = useState('')
 
     let allJobsArray = useLoaderData();
 
@@ -14,11 +16,32 @@ const AppliedJobs = () => {
         applideJosArray = allJobsArray.filter(aJob => storedJosIds.includes(aJob.id))   
     }
 
+    if(filterJobBy){
+        applideJosArray = applideJosArray.filter(aJob => aJob.location_type == filterJobBy || aJob.employment_type == filterJobBy )
+        
+    }
+
     return (
         <div className='container mx-auto my-10'>
+            <div className='text-end'>
+                Filter by
+                
+                <select defaultValue="none" onChange={(e)=>{
+                    setFilterJobBy(e.target.value)
+                }} className='border'>
+                    <option value='' >none</option>
+                    <option value='onsite'>onsite</option>
+                    <option value='remote'>remote</option>
+                    <option value='part-time'>part-time</option>
+                    <option value='full-time'>full-time</option>
+                </select>
+            </div>
+            <div>
+                {(applideJosArray.length > 0) || <p className='h-[400px] border justify-center items-center text-5xl text-red-600 font-bold flex'>Not yet applied</p>}
+                {applideJosArray.length && applideJosArray.map(job => <AppliedJobCard job={job} key={job.id}></AppliedJobCard>)}
+
+            </div>
             {/* {applideJosIds.length || `Not yet applied`} */}
-            {(applideJosArray.length > 0) || <p className='h-[400px] border justify-center items-center text-5xl text-red-600 font-bold flex'>Not yet applied</p>}
-            {applideJosArray.length && applideJosArray.map(job => <AppliedJobCard job={job} key={job.id}></AppliedJobCard>)}
         </div>
     );
 };
